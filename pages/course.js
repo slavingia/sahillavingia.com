9f7f36b4acb9e5e04cea3438526e3240a5d7e6b3
Sahil Lavingia <sahil.lavingia@gmail.com>
2021-01-15 09:40:37 -0800

    cp

--- pages/course.js
+++ pages/course.js
@@ -34,7 +34,7 @@ export default function Home() {
 
         <p>This course is about helping people do that.</p>
 
-        <p>The goal of this course is to help you think differently about entrepreneurship. It exists because I had to think differently about how to construct my life–and my work–before I got happy.</p>
+        <p>The goal of this course is to help you think differently about entrepreneurship. It exists because, after Gumroad came to a <a href="/reflecting">screeching halt in 2015</a>, I had to think differently about how to rebuild my life–and my work–in a way that would make me happy.</p>
 
         <p><img src="/minient.png" /></p>
 
@@ -48,35 +48,35 @@ export default function Home() {
 
         <ol>
           <li>
-            <strong>The minimalist business.</strong>
+            <strong>The minimalist business</strong>
             <p>The real problems with VC-driven startups and IPOs from the perspective of someone who traveled that road (and then skidded right off). Also, on the irony of a “lifestyle” business.</p>
           </li>
           <li>
-            <strong>Community first.</strong>
+            <strong>Community first</strong>
             <p>Designing a vision for your business by working backward from problems worth solving for people worth solving problems for.</p>
           </li>
           <li>
-            <strong>Make something they need.</strong>
+            <strong>Make something they need</strong>
             <p>Leveraging your experiences and assets as an entrepreneur to hit the ground running.</p>
           </li>
           <li>
-            <strong>Sell to your first 100 customers.</strong>
+            <strong>Sell to your first 100 customers</strong>
             <p>Don’t launch, sell. Debunking the myth of “virality” and how to market your business like a grown-up.</p>
           </li>
           <li>
-            <strong>Market like a minimalist.</strong>
+            <strong>Market like a minimalist</strong>
             <p>Turn strangers into fans, and fans into customers. For free.</p>
           </li>
           <li>
-            <strong>Grow sustainably.</strong>
+            <strong>Grow sustainably</strong>
             <p>No profits = no payroll = no business. Letting go of Silicon Valley sloppiness and acing the fundamentals: operations, legal, finance.</p>
           </li>
           <li>
-            <strong>Build the house you want to live in.</strong>
+            <strong>Build the house you want to live in</strong>
             <p>Getting the culture to fit without ever using the word “culture-fit.”</p>
           </li>
           <li>
-            <strong>The moral company.</strong>
+            <strong>The moral company</strong>
             <p>On the ethical significance of providing a product in return for payment. And exploring what comes next.</p>
           </li>
         </ol>
@@ -91,7 +91,7 @@ export default function Home() {
           <li>Creating and leveraging a strong community of customers to kick-start your business’ growth</li>
           <li>Successful sales tactics since sales is the best way to build your early traction, not Facebook ads or viral tricks</li>
           <li>Marketing without costs by turning your strangers into your fans, and fans into customers</li>
-          <li>How to nail fundamentals like operations, legal and finance so you can maintain profitability as you scale</li>
+          <li>How to nail fundamentals like operations, legal, and finance so you can maintain profitability as you scale</li>
           <li>Building a company culture that isn’t about beanbags and snacks</li>
           <li>Using modern workplace options such as remote work and fractional work to build an awesome team without a high full-time headcount</li>
         </ul>
@@ -99,26 +99,26 @@ export default function Home() {
         <p>After taking this course (if not during it!), you will be able to start your own business to serve a community, ship a value-additive product for them, and sell it to a hundred customers.</p>
 
         <h2>Cohort-based courses</h2>
-        <p>There's no way I could do this alone. Besides my co-instructor Julie Moscow, I am working with a <a href="bit.ly/wk-gb">new startup</a> by AltMBA co-founder Wes Kao and Udemy co-founder Gagan Biyani to make this course happen.</p>
+        <p>There's no way I could do this alone. Besides my co-instructor Julie Mosow, I am working with a <a href="bit.ly/wk-gb">new startup</a> by AltMBA co-founder Wes Kao and Udemy co-founder Gagan Biyani to make this course happen.</p>
         <p>The benefits include...</p>
         <ul>
           <li>
-            <strong>Learn with a Cohort of Peers</strong>
-            <p>This course is designed to facilitate peer learning and interaction through Zoom breakout groups, an active online community and group projects</p>
+            <strong>Learn with a cohort of peers</strong>
+            <p>This course is designed to facilitate peer learning and interaction through Zoom breakout groups, an active online community, and group projects.</p>
           </li>
 
           <li>
-            <strong>Active Learning, not Passive Lecture Watching</strong>
-            <p>Sahil won’t just talk at you, but rather engage via live Q&amp;A, so this feels more like an interactive seminar than a massive lecture hall</p>
+            <strong>Active learning, not passive lecture watching</strong>
+            <p>I won’t just talk at you. I will engage via live Q&amp;A, so this feels more like an interactive seminar than a massive lecture hall.</p>
           </li>
 
           <li>
-            <strong>Accountability and Feedback</strong>
+            <strong>Accountability and feedback</strong>
             <p>You’re an entrepreneur. You can figure things out when you have to, but sometimes it’s helpful to have feedback and accountability to increase your chances of success.</p>
           </li>
 
           <li>
-            <strong>Guest Lecturers</strong>
+            <strong>Guest lecturers</strong>
             <p>There'll be guests who will come in and provide different perspectives on how to be a Minimalist Entrepreneur.</p>
           </li>
         </ul>
@@ -126,6 +126,8 @@ export default function Home() {
         <p>But this is ultimately my course, and I will work hard and be present to improve the experience as much as I can.</p>
 
         <h2>Frequently-asked questions</h2>
+
+        <p>If these don't answer your questions, please <a href="mailto:sahil@hey.com">email me</a> or <a href="https://twitter.com/shl">DM me on Twitter</a>.</p>
 
         <ul>
           <li>
@@ -145,8 +147,6 @@ export default function Home() {
             <p>It's at least two hour-long Zoom classes–Tuesdays and Thursdays at 9AM PST–with assignments, every week. The more you put in, the more you'll get out. I would recommend 5-7 hours a week dedicated to this class, if not more.</p>
           </li>
         </ul>
-
-        <p>If you have further questions, please email or DM me on Twitter.</p>
 
         <p>This is a course with limited seats, priced at <strong>$850</strong>. We'll meet every Tuesday and Thursday from February 2nd to February 25th, at 9AM PST.</p>
 
