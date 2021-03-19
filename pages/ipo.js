2febfd111dae7a9948b97cb2a67a7ec7090611e6
Sahil Lavingia <sahil.lavingia@gmail.com>
2021-03-19 12:46:06 -0700

    cp

---
+++ pages/ipo.js
@@ -0,0 +1,93 @@
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+export default function Home() {
+  return (
+    <Layout>
+      <Head>
+        <title>The Creator-Owner Economy: Gumroad’s "IPO"</title>
+        <link rel="icon" href="/favicon.svg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@shl" />
+        <meta name="twitter:title" content="The Creator-Owner Economy" />
+        <meta name="twitter:description" content="Gumroad’s 'IPO'" />
+        <meta name="twitter:image" content="https://sahillavingia.com/c.png" />
+      </Head>
+
+      <a className="tiny-title" href="/">
+        &larr; Sahil Lavingia
+      </a>
+
+      <a className="twitter-link" href="https://twitter.com/shl">
+        <img src="/twitter.png" className="twitter" /> &rarr;
+      </a>
+
+      <main className="centered-main content">
+        <label>
+          March 23, 2021
+        </label>
+
+        <h2 className="item">
+          <strong>The Creator-Owner Economy: Gumroad’s "IPO"</strong>
+        </h2>
+
+        <p>Creators deserve to get paid for their work.</p>
+        <p>To date, <a href="https://gumroad.com">Gumroad</a> has made it easy by enabling creators to sell digital products, online courses, and memberships directly to their audience. But earning an income isn't the only way to get paid.</p>
+        <p>Owning equity in a business is another.</p>
+        <blockquote class="twitter-tweet" data-conversation="none"><p lang="en" dir="ltr">You’re not going to get rich renting out your time. You must own equity - a piece of a business - to gain your financial freedom.</p>&mdash; Naval (@naval) <a href="https://twitter.com/naval/status/1002103670400417792?ref_src=twsrc%5Etfw">May 31, 2018</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>        <h2>Turning creators into owners</h2>
+        <p>In 2011, I raised a $1.1M Seed from Silicon Valley folks like Max Levchin, Chris Sacca, and First Round. In 2012 I raised a $7M Series A led by Kleiner Perkins.</p>
+        <p>I’ve also failed to raise money. In 2015, after meeting with dozens of investors and getting dozens of nos, we wrote off a Series B and had to either sell, shut down, or get acquired. I <a href="/reflecting">wrote about this decision</a>, saying:</p>
+        <blockquote>I was accountable to our creators, our employees, and our investors–in that order.</blockquote>
+        <p>Spoiler alert: I chose our creators over our employees and our investors, deciding not to sell the company. Instead, I shrunk the team from 20 to five in order to keep serving our creators.</p>
+        <p>It was a painful decision, and in the years since I’ve thought about how to make interests align more cleanly. For example, I’ve recently started to <a href="/work">hire some of our top creators</a>.</p>
+        <p>A logical extension: allowing them to become Gumroad investors too.</p>
+        <h2>From income to equity</h2>
+        <p>As of March 15 2021, the SEC allows companies to raise up to $5M from the public, using Regulation Crowdfunding (Reg CF).</p>
+        <p>We took advantage of these new rules as soon as we could, setting a $1K maximum check size due to anticipated demand. Even with that, we hit the new limit in just about 12 hours, from 7,300 investors–including many of our creators.</p>
+        <p><img src="/republic.png" style={{margin: "1rem 0"}} /></p>
+        <p>Including $1M from Naval Ravikant, Jason Fried, and others, the total amount raised is $6M. We raised this capital at a $100M pre-money valuation.</p>
+        <p>I thought about calling this our Series C–for creators, community, crowdfunding, and out of respect for our failed Series B. But it's more accurate to say this is our initial public offering.</p>
+        <p>For 79% of our investors, this was their first-ever angel investment. That is over 5,000 newly-minted angel investors, several who have wanted to do this for a while but have been traditionally excluded.</p>
+        <p>Speaking for myself, it was an important shift for me to go from thinking in terms of earning an income to thinking in terms of owning equity, and I'm excited to see what happens next.</p>
+        <p>The half that's missing: post-IPO liquidity. Good news: Reg CF allows for secondary sales after a 1-year lockup period, and we'll be one of the first to allow our new owners to sell some of their shares to others, if they wish.</p>
+        <p>And in that future, perhaps there's no reason to "go public"–we already have.</p>
+        <h2>The future of fundraising</h2>
+        <p>I hope to see more startups raise money from their customers and communities. And not just startups, but restaurants and coffee shops and bookstores and more mature companies, and even venture capital firms like Arlan Hamilton's Backstage.</p>
+        <p>Why isn’t it more common? First off, the legislation is a day old. So it’ll take some time to get people (both founders and retail investors) educated.</p>
+        <p>There are other reasons too:</p>
+        <ul>
+          <li>It requires a lot of financial transparency–<em>I publish our financials already</em></li>
+          <li>Having thousands of investors may create external pressure around decision-making around things like moderation, deplatforming, and pricing–<em>We’re very open about our decision-making already with our Open Board Meetings, encouraging feedback</em></li>
+          <li>The brand and experience of VCs are often preferred by founders over the general public when selling customers, employees, etc–<em>I’ve raised money from VCs before, and can get the help I need (I’m kind of one myself!)</em></li>
+          <li>You need to have high confidence you can reach a large audience of retail investors–<em>With “testing the waters” now allowed, founders can gauge demand before commiting</em></li>
+        </ul>
+        <p>More businesses will embrace this method to fundraise, because the upside of having customers as owners is too great. Gumroad now has hundreds of customers as owners, who are doubly-incentivized to help make our product better, use the product itself more, and spread the word.</p>
+        <p>This is already the cause in the UK, where equity crowdfunding is more than <a href="https://docs.google.com/spreadsheets/d/1CZXdI0Giq1AGsPEG2HTg2t6zU5MrSazEvrpSQ0kcib0/edit#gid=0">60x the US crowdfunding market</a> (compared to their respective GDPs).</p>
+        <p>To be clear: quintupling the crowdfunding cap is not going to 5x crowdfunding, it is going to 50x crowdfunding. Gumroad did not go from raising $1M to raising $5M, but from not raising this way to raising raising in this way. At least for Gumroad, equity crowdfunding now has product-market fit.</p>
+        <h2>How can I help?</h2>
+        <p>There is one way to make this happen sooner: show founders that taking this approach will make their companies more valuable.</p>
+        <p>So, to Gumroad's 7,303 or so new investors asking "How can I help?" for the very first time:</p>
+        <ul>
+          <li><a href="https://gumroad.com/signup">Sign up for Gumroad as a creator</a></li>
+          <li><a href="https://gumroad.com/discover">Support Gumroad creators as a customer</a></li>
+          <li>Send me and the team feedback–via <a href="https://joinclubhouse.com/club/gumroad">Clubhouse</a>, <a href="https://twitter.com/gumroad">Twitter</a>, and elsewhere</li>
+          <li>Tell people Gumroad exists–Start by sharing this news!</li>
+        </ul>
+
+        <p>And if you're a founder, please check out <a href="https://raise.gumroad.com/">the materials I used</a> to crowdfund this raise, and reach out if you have any questions.</p>
+
+        <hr />
+
+        <label className="centered">
+          Join 20,000 people receiving updates on the Gumroad journey:
+        </label>
+
+        <form action="https://gumroad.com/follow_from_embed_form" className="gumroad-follow-form-embed centered" method="post">
+          <input name="seller_id" type="hidden" value="2614450981218" />
+          <input name="email" placeholder="Your email address" type="email" />
+          <button data-custom-highlight-color="" type="submit">Subscribe</button>
+        </form>
+      </main>
+    </Layout>
+  )
+}
