a507099ebe85f72dc81e6d69d8f5a2ad092d4827
Sahil Lavingia <sahil.lavingia@gmail.com>
2024-05-08 09:52:05 -0400

    Update index.js (#19)

--- pages/index.js
+++ pages/index.js
@@ -13,63 +13,43 @@ export default function Home() {
       <main className="centered-main">
         <Header></Header>
 
-        <div className="mobile-single-column" style={{
-          display: "flex"
-        }}>
-          <div style={{
-            flex: 4
-          }}>
-            <label>
-              Book
-            </label>
+        <label>
+          Writer
+        </label>
 
-            <a href="https://www.amazon.com/Minimalist-Entrepreneur-Great-Founders-More/dp/0593192397">
-              <img src="/book.png" className="book-cover" />
-            </a>
-          </div>
-          <div style={{
-            flex: 8
-          }}>
-            <label>
-              Essays
-            </label>
+        <h2 className="item index-list-of-writings-title">
+          <a href="https://www.amazon.com/Minimalist-Entrepreneur-Great-Founders-More/dp/0593192397">
+            The Minimalist Entrepreneur
+          </a>
+          <small>2021</small>
+        </h2>
 
-            <h2 className="item index-list-of-writings-title">
-              <a href="/reflecting">
-                Reflecting on My Failure to Build a Billion-Dollar Company
-              </a>
-              <small>2019</small>
-            </h2>
+        <h2 className="item index-list-of-writings-title">
+          <a href="/reflecting">
+            Reflecting on My Failure to Build a Billion-Dollar Company
+          </a>
+          <small>2019</small>
+        </h2>
 
-            <h2 className="item index-list-of-writings-title">
-              <a href="/work">
-                No Meetings, No Deadlines, No Full-Time Employees
-              </a>
-              <small>2021</small>
-            </h2>
+        <h2 className="item index-list-of-writings-title">
+          <a href="/work">
+            No Meetings, No Deadlines, No Full-Time Employees
+          </a>
+          <small>2021</small>
+        </h2>
 
-            <h2 className="item index-list-of-writings-title">
-              <a href="/bubble">
-                From Bubble to Bubble
-              </a>
-              <small>2018</small>
-            </h2>
+        <h2 className="item index-list-of-writings-title">
+          <a href="/bubble">
+            From Bubble to Bubble
+          </a>
+          <small>2018</small>
+        </h2>
 
-            <h2 className="item index-list-of-writings-title">
-              <a href="/border">
-                Across the Border
-              </a>
-              <small>Sci-fi, 2018</small>
-            </h2>
-          </div>
-        </div>
-
-        <h2 className="fund">
-          <label>
-            Funder
-          </label>
-
-          Investing $5 million a year into early-stage startups. <a href="https://shl.capital">Co-invest with me?</a>
+        <h2 className="item index-list-of-writings-title">
+          <a href="/border">
+            Across the Border
+          </a>
+          <small>Sci-fi, 2018</small>
         </h2>
 
         <h2 className="paintings">
