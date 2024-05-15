4ad2acc7cccab715ed69c9b2d8115bc0e52f62ba
= <sahil.lavingia@gmail.com>
2024-05-15 11:10:52 -0400

    cp

--- components/layout.js
+++ components/layout.js
@@ -4,14 +4,11 @@ import { GoogleAnalytics } from '@next/third-parties/google';
 import { Analytics } from '@vercel/analytics/react';
 
 const Layout = ({ children }) => {
-  React.useEffect(() => {
-    // Initialize Google Analytics
-    <GoogleAnalytics gaId="G-8EFZ98JM23" />
-  }, []);
-
   return (
     <div className={styles.container}>
       {children}
+      <Analytics />
+      <GoogleAnalytics gaId="G-8EFZ98JM23" />
     </div>
   );
 };
