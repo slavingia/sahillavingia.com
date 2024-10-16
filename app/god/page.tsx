0382ba6dbed1469bd0b5fdf0873264832fb2d82b
Sahil Lavingia <sahil.lavingia@gmail.com>
2024-10-16 16:32:45 -0400

    cp

--- app/god/page.tsx
+++ app/god/page.tsx
@@ -34,15 +34,6 @@ export default function Home() {
       <h1>God Mode</h1>
 
       <p>
-        <em>
-          God mode, a term for a cheat code in video games that makes a player
-          invincible.
-        </em>
-      </p>
-
-      <hr />
-
-      <p>
         A few weeks ago, Paul Graham published{" "}
         <a href="https://www.paulgraham.com/foundermode.html">Founder Mode</a>,
         about a management style practiced by Brian Chesky at Airbnb that was at
