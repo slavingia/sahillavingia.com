dc85b5e6ab631be79f4f1b4afd1518c060934ffb
Sahil Lavingia <sahil.lavingia@gmail.com>
2024-10-16 11:19:40 -0400

    cp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            p: {
-              marginTop: '2rem',
-              marginBottom: '2rem',
-              fontSize: '1.125rem',
-              lineHeight: '1.75rem',
-              color: 'rgb(55 65 81)', // Tailwind's gray-700
-            },
-            h1: {
-              fontSize: '2.25rem',
-              fontWeight: '700',
-              marginBottom: '1rem',
-            },
-            h2: {
-              fontSize: '1.875rem',
-              fontWeight: '600',
-              marginTop: '2rem',
-              marginBottom: '1rem',
-            },
-            a: {
-              color: 'inherit',
-              textDecoration: 'underline',
-              '&:hover': {
-                color: 'currentColor',
-              },
-            },
-            // Add more element styles as needed
-          },
-        },
-      },
-    },
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
