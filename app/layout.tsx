b9173f696f9a7a7f4e47dba95ffcdcbec0fb823d
Arjun Aditya <meow@nermalcat69.dev>
2024-12-30 14:48:38 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,33 @@
 import "../styles/globals.css";
+import { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Sahil Lavingia",
+  description: "Sahil Lavingia is a writer, painter, and founder.",
+  openGraph: {
+    title: "Sahil Lavingia",
+    description: "Sahil Lavingia is a writer, painter, and founder.",
+    images: ["https://sahillavingia.com/og.png"],
+  },
+  twitter: {
+    title: "Sahil Lavingia",
+    description: "Sahil Lavingia is a writer, painter, and founder.",
+    images: ["https://sahillavingia.com/og.png"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+  },
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
