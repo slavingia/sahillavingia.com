854a5add78dbe9ccd2ac703613f5cd1685d43353
Arjun Aditya <meow@nermalcat69.dev>
2025-01-01 04:14:46 +0530

    remove .mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,26 @@ import createMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
 
 const withMDX = createMDX({
-    extension: /\.mdx?$/,
-    options: {
-        remarkPlugins: [remarkGfm],
-        rehypePlugins: [],
-    },
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sahillavingia.com',
+      },
+    ],
+    unoptimized: false,
+  },
+  swcMinify: true,
 }
 
 export default withMDX(nextConfig) 
